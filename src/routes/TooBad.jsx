81635554dc08054039cbf627139e1505725c30c1
Alex Kuchar <hello@alexkuchar.com>
2023-12-27 21:24:21 +0100

    Added too bad page

---
+++ src/routes/TooBad.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TooBad = () => {
+  return (
+    <div className="toobad">
+      <h2>Ohje..</h2>
+      <p>
+        Du scheinst ein problem zu haben. Doch leider ist der Bus, mit den
+        Leuten die es interessiert schon weg.
+      </p>
+    </div>
+  );
+};
+
+export default TooBad;
