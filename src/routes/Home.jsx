63783224f44df79055cdb0eb3cdfea42ab62cd47
Alex Kuchar <hello@alexkuchar.com>
2023-12-27 16:26:58 +0100

    Created Home.jsx

---
+++ src/routes/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>PuttParty</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos beatae rem
+        libero labore distinctio, possimus nulla repellendus dolorum obcaecati
+        sequi fugit voluptatibus voluptate! Et, labore ut harum sequi modi ex
+        optio, soluta totam excepturi libero veritatis voluptate recusandae cum
+        exercitationem.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
