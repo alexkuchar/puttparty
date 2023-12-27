da1c589d467eef654e20ce464bbf023ce72922f8
Alex Kuchar <hello@alexkuchar.com>
2023-12-27 21:22:23 +0100

    Added all necessary routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './assets/styles/_main.scss';
 
 import Home from './routes/Home';
+import Rules from './routes/Rules';
 import ErrorPage from './routes/ErrorPage';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './routes/About';
+import TooBad from './routes/TooBad';
 
 const router = createBrowserRouter([
   { path: '/', element: <Home />, errorElement: <ErrorPage /> },
+  { path: '/regeln', element: <Rules /> },
+  { path: '/ueber', element: <About /> },
+  { path: '/pech', element: <TooBad /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
